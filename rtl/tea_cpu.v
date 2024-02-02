b8d6c1f0a4738ec17fda6e18f56fc15fe0f80df9
huangjie <huangjielg@gmail.com>
2024-02-02 15:51:38 +0800

    111

--- rtl/tea_cpu.v
+++ rtl/tea_cpu.v
@@ -10,9 +10,52 @@ module regfile
     reg [7:0] mem[2**REGFILE_SIZE_WIDTH-1:0];
     always@(posedge clk)begin
         if(we)
-            mem[addr]<=din;
+            mem[addr]<=#1 din;
     end
     assign dout=mem[addr];
+`ifdef SIM
+    reg [7:0] mem00;
+    reg [7:0] mem01;
+    reg [7:0] mem02;
+    reg [7:0] mem03;
+    reg [7:0] mem04;
+    reg [7:0] mem05;
+    reg [7:0] mem06;
+    reg [7:0] mem07;
+    reg [7:0] mem08;
+    reg [7:0] mem09;
+    reg [7:0] mem0a;
+    reg [7:0] mem0b;
+    reg [7:0] mem0c;
+    reg [7:0] mem0d;
+    reg [7:0] mem0e;
+    reg [7:0] mem0f;   
+        always@(posedge clk)begin
+        if(we)begin
+            case(addr)
+                5'h00:mem00<=din;
+                5'h01:mem01<=din;
+                5'h02:mem02<=din;
+                5'h03:mem03<=din;
+                5'h04:mem04<=din;
+                5'h05:mem05<=din;
+                5'h06:mem06<=din;
+                5'h07:mem07<=din;
+                
+                5'h08:mem08<=din;
+                5'h09:mem09<=din;
+                5'h0a:mem0a<=din;
+                5'h0b:mem0b<=din;
+                5'h0c:mem0c<=din;
+                5'h0d:mem0d<=din;
+                5'h0e:mem0e<=din;
+                5'h0f:mem0f<=din;
+
+            endcase
+        end
+    end
+
+    `endif
 endmodule
 
 module call_stack_mem
@@ -27,7 +70,7 @@ module call_stack_mem
     reg [PC_WIDTH-1:0] mem[15:0];
     always@(posedge clk)begin
         if(we)
-            mem[addr]<=din;
+            mem[addr]<=#1 din;
     end
     assign dout=mem[addr];
 endmodule
@@ -56,18 +99,18 @@ module     tea_cpu
 
     reg [PC_WIDTH-1:0] pc;
     reg [PC_WIDTH-1:0] pc_next;
-    assign instr_addr=pc_next;
+    assign instr_addr=pc;
     reg [7:0] acc;
     reg       cy;
     reg phase;
     always@(posedge clk)begin
         if(rst)begin
-            pc<=10'h0;
-            phase<=1'b0;
+            pc<= #1 10'h0;
+            phase<=#1 1'b0;
         end else begin
-            phase<=~phase;
+            phase<=#1  ~phase;
             if(phase)begin
-                pc<=pc_next;
+                pc<=#1 pc_next;
             end
         end
     end
@@ -79,29 +122,29 @@ module     tea_cpu
     wire instr_store = (!instr[8])&&(instr[7:5]==3'h4);
     wire instr_load  = (!instr[8])&&(instr[7:5]==3'h5);
     wire instr_xor   = (!instr[8])&&(instr[7:5]==3'h6);
-    wire instr_sl1   =  (!instr[8])&&(instr[7:5]==3'h7) && !instr[4] &&(instr[3:0==4'h0]);
-    wire instr_sr1   =  (!instr[8])&&(instr[7:5]==3'h7) && instr[4] &&(instr[3:0==4'h0]);
+    wire instr_sl1   =  (!instr[8])&&(instr[7:5]==3'h7) && (!instr[4]) &&(instr[3:0]==4'h0);
+    wire instr_sr1   =  (!instr[8])&&(instr[7:5]==3'h7) && instr[4] &&(instr[3:0]==4'h0);
 
 
-    wire instr_call    = (!instr[8])&&(instr[7:5]==3'h7) && !instr[4] && (instr[3:0]==4'h1);
-    wire instr_callc   = (!instr[8])&&(instr[7:5]==3'h7) && instr[4] && (instr[3:0]==4'h1);
-    wire instr_jump    = (!instr[8])&&(instr[7:5]==3'h7) && !instr[4] && (instr[3:0]==4'h2);
-    wire instr_jumpc   = (!instr[8])&&(instr[7:5]==3'h7) && instr[4] && (instr[3:0]==4'h2);
-    wire instr_return  = (!instr[8])&&(instr[7:5]==3'h7) && !instr[4] && (instr[3:0]==4'h3);
-    wire instr_returnc = (!instr[8])&&(instr[7:5]==3'h7) && instr[4] && (instr[3:0]==4'h3);
+    wire instr_call    = (!instr[8])&&(instr[7:5]==3'h7) && (!instr[4]) && (instr[3:0]==4'h1);
+    wire instr_callc   = (!instr[8])&&(instr[7:5]==3'h7) && (instr[4] )&& (instr[3:0]==4'h1);
+    wire instr_jump    = (!instr[8])&&(instr[7:5]==3'h7) && (!instr[4]) && (instr[3:0]==4'h2);
+    wire instr_jumpc   = (!instr[8])&&(instr[7:5]==3'h7) && (instr[4] )&& (instr[3:0]==4'h2);
+    wire instr_return  = (!instr[8])&&(instr[7:5]==3'h7) && (!instr[4]) && (instr[3:0]==4'h3);
+    wire instr_returnc = (!instr[8])&&(instr[7:5]==3'h7) && (instr[4]) && (instr[3:0]==4'h3);
 
     wire instr_imm     =  (instr[8]==1'b1);
-    wire instr_io      =  (instr==9'h1_e4);
+    wire instr_io      =  (instr==9'h0_e4);
 
 
     reg ioop_r;
 
     always@(posedge clk)begin
         if(rst)begin
-            ioop_r<=1'b0;
+            ioop_r<=#1 1'b0;
         end else begin
             if(phase)begin
-                ioop_r<= instr_io;
+                ioop_r<= #1 instr_io;
             end
         end
     end
@@ -109,19 +152,21 @@ module     tea_cpu
     wire reg_we = instr_store&&!ioop_r&&phase;
 
     reg [7:0] reg_off;
-    assign      reg_addr=reg_off+instr[REGFILE_SIZE_WIDTH-1:0];
+    assign    reg_addr=reg_off+instr[REGFILE_SIZE_WIDTH-1:0];
 
     regfile u_reg
     (
         .clk(clk),
         .din(acc),
-        .we(reg_we),
+        .we(reg_we &&(reg_addr!=reg_addr_last)),
         .dout(reg_value),
         .addr(reg_addr)
     );
     always@(posedge clk)begin
-        if(reg_we&&reg_addr==reg_addr_last)begin
-            reg_off<=reg_value;
+        if(rst)begin
+            reg_off<=#1 8'h0;
+        end else if(reg_we&&reg_addr==reg_addr_last)begin
+            reg_off<=#1 reg_value;
         end
     end
 
@@ -130,24 +175,27 @@ module     tea_cpu
         if(phase)begin
             if(instr[8]==1'b0) begin
                 case(instr[7:5])
-                    3'h0: {cy,acc}<={1'b0,acc}+reg_value+cy; // addcy
-                    3'h1: {cy,acc}<={1'b0,acc}-reg_value-cy; // subcy
-                    3'h2: {cy,acc}<={1'b0,acc&reg_value};    // and
-                    3'h3: {cy,acc}<={1'b0,acc|reg_value};    // or
-                    3'h5: {cy,acc}<={1'b0,ioop_r?io_rddata:reg_value};        // load
-                    3'h6: {cy,acc}<={1'b0,acc^reg_value};    // xor
+                    3'h0: {cy,acc}<=#1 {1'b0,acc}+reg_value+cy; // addcy
+                    3'h1: {cy,acc}<=#1 {1'b0,acc}-reg_value-cy; // subcy
+                    3'h2: {cy,acc}<=#1 {cy,acc&reg_value};    // and
+                    3'h3: {cy,acc}<=#1 {cy,acc|reg_value};    // or
+                    3'h5: {cy,acc}<=#1 {cy,ioop_r?io_rddata:reg_value};        // load
+                    3'h6: {cy,acc}<=#1 {cy,acc^reg_value};    // xor
                     3'h7: begin
                         if(instr[4:0]==5'h0_0)begin
-                            {cy,acc}<={acc,cy};            //sl1
+                            {cy,acc}<=#1 {acc,cy};            //sl1
                         end
                         if(instr[4:0]==5'h1_0)begin
-                            {acc,cy}<={cy,acc};            // sr1
+                            {acc,cy}<=#1 {cy,acc};            // sr1
+                        end
+                        if(instr[4:0]==5'h1_4)begin            //clr c
+                            {acc,cy}<=#1 {acc,1'b0};            // sr1
                         end
                     end
                     default:;
                 endcase
             end else begin
-                acc<=instr[7:0];
+                {cy,acc}<=#1 {cy,instr[7:0]};
             end
         end
     end
@@ -163,13 +211,13 @@ module     tea_cpu
     wire[3:0]  call_stack_addr;
     always@(posedge clk)begin
         if(rst)begin
-            sp<=4'h0;
+            sp<=#1 4'h0;
         end else begin
-            if(instr_call||(instr_callc&&cy))begin
-                sp<=sp+1'b1;
+            if(instr_call||(instr_callc&&(cy^instr[3])))begin
+                sp<=#1 sp+1'b1;
             end
-            if(instr_return||(instr_returnc&&cy))begin
-                sp<=sp-1'b1;
+            if(instr_return||(instr_returnc&&(cy^instr[3])))begin
+                sp<=#1 sp-1'b1;
             end
         end
     end
@@ -182,18 +230,18 @@ module     tea_cpu
         .clk(clk),
         .addr(call_stack_addr),
         .din(pc_next),
-        .we(instr_call||(instr_callc&&cy)),
+        .we(instr_call||(instr_callc&&(cy^instr[3]))),
         .dout(call_stack_top)
     );
 
     always@(*)begin
         pc_next=pc+1'b1;
         if( instr_call||(instr_callc&&cy)
-                ||  instr_jump||(instr_jumpc&&cy)
+                ||  instr_jump||(instr_jumpc&&(cy^instr[3]))
             )begin
-            pc_next=pc+$signed(acc);
+            pc_next=$signed(pc)+$signed(acc);
         end
-        if(instr_return||(instr_returnc&&cy))begin
+        if(instr_return||(instr_returnc&&(cy^instr[3])))begin
             pc_next=call_stack_top;
         end
     end
@@ -204,8 +252,8 @@ endmodule
 // 0_001x_xxxx : subc
 // 0_010x_xxxx : and
 // 0_011x_xxxx : or
-// 0_1000_xxxx : store
-// 0_1010_xxxx : load
+// 0_100x_xxxx : store
+// 0_101x_xxxx : load
 // 0_110x_xxxx : xor
 
 // 0_1110_0000 : sl0
@@ -213,13 +261,17 @@ endmodule
 
 // 0_1110_0001 : call
 // 0_1111_0001 : callc
+// 0_1111_1001 : callnc
 // 0_1110_0010 : jmp
 // 0_1111_0010 : jumpc
+// 0_1111_1010 : jumpnc
 
 // 0_1110_0011 : return
 // 0_1111_0011 : returnc
+// 0_1111_1011 : returnc
 
 // 0_1110_0100 : ioop
+// 0_1111_0100 : clr_c
 
 // 1_xxxx_xxxx : imm
 
--- tb/tb_top.v
+++ tb/tb_top.v
@@ -1,4 +1,5 @@
 `timescale 1ns/1ps
+`define SIM
 `include "../rtl/tea_cpu.v"
 module tb_top;
     reg clk=1'b1;
@@ -10,15 +11,24 @@ module tb_top;
     wire [7:0]  io_wrdata;
     wire [9:0]  instr_addr;
     reg [8:0]  instr;
-    reg [8:0]   instr_rom[1023:0];
-
+    reg [8:0]   rom[1023:0];
+    reg tea_req=1'b1;
+    reg       tea_done=1'b0;
+    reg [7:0] tea_r0;
+    reg [7:0] tea_r1;
+    reg [7:0] tea_r2;
+    reg [7:0] tea_r3;
+    reg [7:0] tea_r4;
+    reg [7:0] tea_r5;
+    reg [7:0] tea_r6;
+    reg [7:0] tea_r7;
     always@(posedge clk)begin
-        instr<=instr_rom[instr_addr];
+        instr<=rom[instr_addr];
     end
     initial forever begin
-        clk<= 1'b1;#5;
-        clk<= 1'b0;#5;
-    end
+            clk<= 1'b1;#5;
+            clk<= 1'b0;#5;
+        end
     initial begin
         rst<=1'b1;
         repeat(10) @(posedge clk);
@@ -37,8 +47,135 @@ module tb_top;
         .instr_addr(instr_addr),
         .instr(instr)
     );
+
+    always@(*)begin
+        io_rddata<=8'h0;
+        case(io_addr)
+            5'h0:io_rddata<= 8'h12;
+            5'h1:io_rddata<= 8'h34;
+            5'h2:io_rddata<= 8'h56;
+            5'h3:io_rddata<= 8'h78;
+            5'h4:io_rddata<= 8'h11;
+            5'h5:io_rddata<= 8'h22;
+            5'h6:io_rddata<= 8'h33;
+            5'h7:io_rddata<= 8'h44;
+            5'h1F:io_rddata<= tea_req;
+        endcase
+    end
+    always@(posedge clk)begin
+        if(io_wr)begin
+            case(io_addr)
+                5'h0:tea_r0 <= io_wrdata;
+                5'h1:tea_r1 <= io_wrdata;
+                5'h2:tea_r2 <= io_wrdata;
+                5'h3:tea_r3 <= io_wrdata;
+                5'h4:tea_r4 <= io_wrdata;
+                5'h5:tea_r5 <= io_wrdata;
+                5'h6:tea_r6 <= io_wrdata;
+                5'h7:tea_r7 <= io_wrdata;
+                5'h1F:tea_done<= io_wrdata[0];
+            endcase
+        end
+    end
     initial begin
+        @(negedge rst);
+        repeat(50)  @(posedge clk);
+        tea_req=1'b0;
+        @(posedge tea_done);
+        repeat(10)  @(posedge clk);
+        $finish();
+    end
+    initial begin
+        $fsdbDumpvars();
         #10_000;
         $finish();
     end
+    integer i=0;
+    integer j=0;
+    localparam K0=32'h11_12_13_14;
+    localparam K1=32'h21_22_23_24;
+    localparam K2=32'h31_32_33_34;
+    localparam K3=32'h41_42_43_44;
+
+    function [31:0] sel_k;
+        input [1:0]k_sel;
+        begin
+            case(k_sel)
+                2'h0:sel_k=K0;
+                2'h1:sel_k=K1;
+                2'h2:sel_k=K2;
+                2'h3:sel_k=K3;
+            endcase
+        end
+    endfunction
+
+
+    task  v_add_sum;
+        input [4:0] offset_in;
+        input [4:0] offset_out;
+        begin
+
+            rom[i] = 9'hf4; i=i+1;   // clr_c
+            for(j=0;j<4;j=j+1) begin
+                rom[i] = 9'ha0+offset_in+j;i=i+1; // acc = @offset_in
+                rom[i] = 9'h0_08+j;i=i+1;  // acc <= acc+ sum08
+                rom[i] = 9'h0_80+offset_out+j;i=i+1;  // reg08 <= acc
+            end
+        end
+    endtask
+
+    task  v_sl4_add_k;
+        input [4:0] offset_v;
+        input [4:0] offset_temp;
+        input [31:0] k;
+        begin
+
+
+        end
+    endtask
+
+
+    initial begin
+        i=0;
+        // 反复读取状态，等待有一个请求
+        rom[i]=9'he4; i=i+1; // io req
+        rom[i]=9'ha0+9'h1f;i=i+1;// read from io
+        rom[i]=9'hf0;i=i+1;// sr1
+        rom[i]={1'b1,8'hfc};i=i+1;// acc<=-4
+        rom[i]=9'hf2;i=i+1; //jump c
+        //把输入值读入内部寄存器
+        for(j=0;j<8;j=j+1) begin
+            rom[i]=9'he4;i=i+1;
+            rom[i]=9'ha0+9'h0+j;i=i+1;// read from io
+            rom[i]=9'h80+0+j;i=i+1;   // store to 0
+        end
+        rom[i] = 9'h1_00; i=i+1; // acc<=0;
+        rom[i] = 9'h80+8;i=i+1;  // sum 0
+        rom[i] = 9'h80+9;i=i+1;  // sum 0
+        rom[i] = 9'h80+10;i=i+1;  // sum 0
+        rom[i] = 9'h80+11;i=i+1;  // sum 0
+
+        rom[i] = 9'hf4; i=i+1;// clr_c
+
+        rom[i] = 9'h1_b9;i=i+1;  // acc <= b9
+        rom[i] = 9'h0_08;i=i+1;  // acc <= acc+ reg08
+        rom[i] = 9'h0_88;i=i+1;  // reg08 <= acc
+
+        rom[i] = 9'h1_79;i=i+1;  // acc <= 79
+        rom[i] = 9'h0_09;i=i+1;  // acc <= acc+ reg09
+        rom[i] = 9'h0_89;i=i+1;  // reg09 <= acc
+
+        rom[i] = 9'h1_37;i=i+1;  // acc <= 37
+        rom[i] = 9'h0_0a;i=i+1;  // acc <= acc+ reg0a
+        rom[i] = 9'h0_8a;i=i+1;  // reg0a <= acc
+
+        rom[i] = 9'h1_9e;i=i+1;  // acc <= 9e
+        rom[i] = 9'h0_0b;i=i+1;  // acc <= acc+ reg0b
+        rom[i] = 9'h0_8b;i=i+1;  // reg0b <= acc
+
+        //v1+sum
+        v_add_sum(4,12);
+
+    end
+
 endmodule
